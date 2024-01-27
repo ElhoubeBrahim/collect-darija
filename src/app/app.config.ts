48b588b45b1e04bed27999318403455f2dbd97e0
ElhoubeBrahim <brahim.elhoube@gmail.com>
2024-01-27 23:16:21 +0100

    setup firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,30 @@
-import { ApplicationConfig } from "@angular/core";
+import { ApplicationConfig, importProvidersFrom } from "@angular/core";
 import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { getAuth, provideAuth } from "@angular/fire/auth";
+import {
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from "@angular/fire/analytics";
+import { getFirestore, provideFirestore } from "@angular/fire/firestore";
+import { environment } from "../environments/environment.development";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp(environment.firebaseConfig),
+      ),
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideAnalytics(() => getAnalytics())),
+    ScreenTrackingService,
+    UserTrackingService,
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+  ],
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    authDomain: "project-id.firebaseapp.com",
+    projectId: "project-id",
+    storageBucket: "project-id.appspot.com",
+    messagingSenderId: "XXXXXXXXXXXX",
+    appId: "X:XXXXXXXXXXXX:web:XXXXXXXXXXXXXXXXXXXXXXX",
+    measurementId: "G-XXXXXXXXXX",
+  },
+};
