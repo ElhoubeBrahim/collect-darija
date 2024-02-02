db3c9b63f10eb7522a066255e954c16c4935894d
ElhoubeBrahim <brahim.elhoube@gmail.com>
2024-02-02 21:56:24 +0100

    add router link to logo in the header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,11 +2,12 @@ import { Component } from "@angular/core";
 import { Store, select } from "@ngrx/store";
 import { userSelector } from "../../store/authentication/authentication.selector";
 import { AsyncPipe, JsonPipe } from "@angular/common";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: "app-header",
   standalone: true,
-  imports: [AsyncPipe, JsonPipe],
+  imports: [AsyncPipe, JsonPipe, RouterLink],
   templateUrl: "./header.component.html",
 })
 export class HeaderComponent {
