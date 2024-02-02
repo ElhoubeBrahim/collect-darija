0d00edd30f4ced2c756df1d30a6c01c9a2f25605
ElhoubeBrahim <brahim.elhoube@gmail.com>
2024-02-02 23:01:21 +0100

    add user avatar dropdown in the header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,6 +3,7 @@ import { Store, select } from "@ngrx/store";
 import { userSelector } from "../../store/authentication/authentication.selector";
 import { AsyncPipe, JsonPipe } from "@angular/common";
 import { RouterLink } from "@angular/router";
+import { AuthenticationService } from "../../services/authentication.service";
 
 @Component({
   selector: "app-header",
@@ -12,6 +13,26 @@ import { RouterLink } from "@angular/router";
 })
 export class HeaderComponent {
   user$ = this.store.pipe(select(userSelector));
+  dropdown = {
+    open: false,
+    links: [
+      { icon: "home-7-line", label: "Home", route: "/home" },
+      { icon: "translate", label: "Translate", route: "/translate" },
+      { icon: "trophy-line", label: "Leaderboard", route: "/leaderboard" },
+    ],
+  };
 
-  constructor(private store: Store) {}
+  constructor(
+    private store: Store,
+    private authentication: AuthenticationService,
+  ) {}
+
+  async logout() {
+    if (confirm("Are you sure you want to log out?") === false) return;
+    await this.authentication.logout();
+  }
+
+  toggleDropdown() {
+    this.dropdown.open = !this.dropdown.open;
+  }
 }
