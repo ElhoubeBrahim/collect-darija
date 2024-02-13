248382eb1d321921ecc70485f72e2b79650439ce
ElhoubeBrahim <brahim.elhoube@gmail.com>
2024-02-13 22:21:28 +0100

    use angularfire's latest api in the leaderboard service

--- src/app/services/leaderboard.service.ts
+++ src/app/services/leaderboard.service.ts
@@ -1,32 +1,55 @@
 import { Injectable } from "@angular/core";
-import { AngularFirestore } from "@angular/fire/compat/firestore";
 import { User, UserWithRanking } from "../models/users.model";
 import { Observable, lastValueFrom } from "rxjs";
 import { AuthenticationService } from "./authentication.service";
-import { ToastrService } from "ngx-toastr";
 import { Translation } from "../models/translations.model";
+import { Firestore } from "@angular/fire/firestore";
+import {
+  collection,
+  getDocs,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
 
 @Injectable({
   providedIn: "root",
 })
 export class LeaderboardService {
   private LEADERBOARD_SIZE = 10;
+  private MAX_DOCUMENTS = 1000;
 
   constructor(
-    private firestore: AngularFirestore,
+    private firestore: Firestore,
     private authentication: AuthenticationService,
   ) {}
 
   getLeaderboard(): Observable<User[]> {
-    return this.firestore
-      .collection<User>("users", (ref) =>
-        ref
-          .orderBy("score", "desc")
-          .orderBy("scoreUpdatedAt", "asc")
-          .limit(this.LEADERBOARD_SIZE)
-          .where("score", ">", 0),
-      )
-      .valueChanges();
+    return new Observable((observer) => {
+      // Get the users collection
+      const usersCollection = collection(this.firestore, "users");
+
+      // Get the users ordered by score
+      const q = query(
+        usersCollection,
+        orderBy("score", "desc"),
+        orderBy("scoreUpdatedAt", "asc"),
+        limit(this.LEADERBOARD_SIZE),
+        where("score", ">", 0),
+      );
+
+      // Subscribe to the users collection
+      const unsubscribe = onSnapshot(q, (querySnapshot) => {
+        const leaderboard = querySnapshot.docs.map((doc) =>
+          doc.data(),
+        ) as User[];
+        observer.next(leaderboard);
+      });
+
+      return () => unsubscribe();
+    });
   }
 
   async setupLeaderboard(users: User[]): Promise<UserWithRanking[]> {
@@ -41,7 +64,7 @@ export class LeaderboardService {
     // If the current user is not in the leaderboard
     if (user && user.score > 0 && !leaderboard.find((u) => u.id === user.id)) {
       // Get the current user's ranking
-      const ranking = await this.getCurrentUserRanking();
+      const ranking = await this.getUserRanking(user);
       // Add the current user to the leaderboard
       ranking > this.LEADERBOARD_SIZE && leaderboard.push({ ...user, ranking });
     }
@@ -50,31 +73,26 @@ export class LeaderboardService {
     return leaderboard;
   }
 
-  async getCurrentUserRanking(): Promise<number> {
-    // Get current user
-    const user = await this.authentication.getCurrentUser();
-    if (!user || user.score == 0) {
-      return 0;
-    }
+  async getUserRanking(user: User): Promise<number> {
+    // Check current user's score
+    if (!user || user.score == 0) return 0;
 
-    // Get users with a higher score
-    const users$ = this.firestore
-      .collection("users", (ref) =>
-        ref
-          .orderBy("score", "desc")
-          .orderBy("scoreUpdatedAt", "asc")
-          .where("score", ">=", user.score),
-      )
-      .get()
-      .pipe();
+    // Get all users with a score higher or equal to the current user
+    const usersCollection = collection(this.firestore, "users");
+    const q = query(
+      usersCollection,
+      orderBy("score", "desc"),
+      orderBy("scoreUpdatedAt", "asc"),
+      where("score", ">=", user.score),
+      limit(this.MAX_DOCUMENTS),
+    );
+    const usersSnapshot = await getDocs(q);
 
-    // Return the number of users with a higher score
-    const users = await lastValueFrom(users$);
-    return users.docs.findIndex((u) => u.id === user.id) + 1;
+    // Return the current user's ranking
+    return usersSnapshot.docs.findIndex((u) => u.id === user.id) + 1;
   }
 
   async getWeeklyContributions(): Promise<{ day: Date; value: number }[]> {
-    console.log("getWeeklyContributions");
     // Get the current user
     const user = await this.authentication.getCurrentUser();
     if (!user) return [];
@@ -87,19 +105,19 @@ export class LeaderboardService {
     ];
 
     // Get the translations
-    const translations$ = this.firestore
-      .collection<Translation>("translations", (ref) =>
-        ref
-          .where("userId", "==", user.id)
-          .where("translatedAt", ">=", range[0])
-          .where(
-            "translatedAt",
-            "<=",
-            new Date(range[1].getTime() + 24 * 60 * 60 * 1000),
-          ),
-      )
-      .get();
-    const translations = await lastValueFrom(translations$);
+    const translationsCollection = collection(this.firestore, "translations");
+    const q = query(
+      translationsCollection,
+      where("userId", "==", user.id),
+      where("translatedAt", ">=", range[0]),
+      where(
+        "translatedAt",
+        "<=",
+        new Date(range[1].getTime() + 24 * 60 * 60 * 1000),
+      ),
+    );
+    const translationsSnapshot = await getDocs(q);
+    const translations = translationsSnapshot.docs;
 
     // Count contributions
     const contributions = [];
@@ -112,11 +130,10 @@ export class LeaderboardService {
       );
       contributions.push({
         day: date,
-        value: translations.docs.filter(
-          (t) =>
-            t.data().translatedAt.toDate().toDateString() ===
-            date.toDateString(),
-        ).length,
+        value: translations.filter((translation) => {
+          const t = translation.data() as Translation;
+          return t.translatedAt.toDate().toDateString() === date.toDateString();
+        }).length,
       });
     }
 
