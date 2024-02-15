bb8e4cd401b904111fdbb3c94949bf906b7d54c7
elqessouartariq <tariqelqessouar@gmail.com>
2024-02-15 19:11:27 +0100

    feat: add HistoryTableComponent

--- src/app/components/history-table/history-table.component.ts
+++ src/app/components/history-table/history-table.component.ts
@@ -1,9 +1,20 @@
 import { Component } from "@angular/core";
+import { MatDialog, MatDialogModule } from "@angular/material/dialog";
+import { MatButtonModule } from "@angular/material/button";
+import { HistoryModalContentComponent } from "../history-modal-content/history-modal-content.component";
 
 @Component({
   selector: "app-history-table",
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatDialogModule, HistoryModalContentComponent],
   templateUrl: "./history-table.component.html",
 })
-export class HistoryTableComponent {}
+export class HistoryTableComponent {
+  constructor(public dialog: MatDialog) {}
+
+  openDialog() {
+    const dialogRef = this.dialog.open(HistoryModalContentComponent, {
+      maxWidth: "98vw",
+    });
+  }
+}
