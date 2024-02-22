8ad1cd3dce730b44f7a2ac0d10c0db3421388041
ElhoubeBrahim <brahim.elhoube@gmail.com>
2024-02-22 09:48:43 +0100

    submit translation to the api

--- src/app/services/translation.service.ts
+++ src/app/services/translation.service.ts
@@ -3,17 +3,13 @@ import { Sentence } from "../models/sentences.model";
 import { AuthenticationService } from "./authentication.service";
 import { ToastrService } from "ngx-toastr";
 import { Translation } from "../models/translations.model";
-import { Timestamp } from "@firebase/firestore";
 import {
   Firestore,
   collection,
-  doc,
   getDocs,
-  increment,
   limit,
   orderBy,
   query,
-  runTransaction,
   where,
 } from "@angular/fire/firestore";
 import { HttpClient } from "@angular/common/http";
@@ -40,48 +36,11 @@ export class TranslationService {
   }
 
   async translateSentence(sentence: Sentence, translation: string) {
-    // Get logged in user
-    const user = await this.authentication.getCurrentUser();
-    if (!user) {
-      this.toastr.error("Please login to translate sentences!");
-      return;
-    }
-
-    // Create a new translation
-    const translationData: Translation = {
-      id: doc(collection(this.firestore, "translations")).id, // Generate a new id
-      userId: user.id,
-      sentence: {
-        id: sentence.id,
-        content: sentence.content,
-      },
+    const observable$ = this.http.post("/translate", {
+      sentenceId: sentence.id,
       translation,
-      translatedAt: Timestamp.fromDate(new Date()),
-    };
-
-    await runTransaction(this.firestore, async (transaction) => {
-      // Save the translation
-      const translationDoc = doc(
-        this.firestore,
-        "translations",
-        translationData.id,
-      );
-      transaction.set(translationDoc, translationData);
-
-      // Update the sentence translations count
-      const sentenceDoc = doc(this.firestore, "sentences", sentence.id);
-      transaction.update(sentenceDoc, {
-        translationsCount: increment(1),
-      });
-
-      // Update user translations count & score
-      const userDoc = doc(this.firestore, "users", user.id);
-      transaction.update(userDoc, {
-        "stats.translations": increment(1),
-        score: increment(10),
-        scoreUpdatedAt: Timestamp.fromDate(new Date()),
-      });
     });
+    await lastValueFrom(observable$); // To make the request
   }
 
   async getTranslationsHistory(count = 10): Promise<Translation[]> {
